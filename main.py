26c3b71d27a341e2bd6e230a95140ac0692ae114
Naomi Broersma <naomibroersma.nb@gmail.com>
2022-10-24 12:57:37 +0200

    Working on creating the right structure of the graph

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import networkx
+import networkx as nx
+import matplotlib.pyplot as plt
 
 from preprocessing import read_data_file
 
--- preprocessing.py
+++ preprocessing.py
@@ -5,7 +5,7 @@ import os
 # Reads the data from the structure.json file.
 # TODO how to save the data to get the full correct structure?
 # TODO input to save files for rumour and non-rumour?
-def read_data_file():
+def read_data_file(graph):
     # Get the current working directory and ensure we are on the correct path.
     wd = os.getcwd()
     print("Current working directory: {0}".format(wd))
@@ -27,10 +27,28 @@ def read_data_file():
                         data = json.load(f)
                         print(data)
                         for key in data:
-                            # Prints the source tweet.
-                            print(key)
-                            # Prints the tweets linked to the source tweet and their inner structure.
-                            # TODO how to get the structure if tweets continue further?
-                            print(data[key])
+                            graph = determine_nodes_and_edges(graph, key, data[key])
                         # Closing file
                         f.close()
+    graph.add_weighted_edges_from([(553187282295877632, 553187515671138305, 1)])
+    print(graph)
+    pos = nx.random_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+    plt.savefig("graph.png")
+    plt.show()
+
+#check if weight exists, if so add 1 to the weight (weight increases with interaction between two users)
+
+
+def determine_nodes_and_edges(graph, main_node, values):
+    # Prints the source tweet.
+    print(main_node)
+    graph.add_node(main_node)
+    # Prints the tweets linked to the source tweet and their inner structure.
+    # TODO how to get the structure if tweets continue further?
+    print(values)
+    out = re.split(r"[:|,]", str(values))
+    print(out)
+    for elem in out:
+        print(elem)
+    return graph
