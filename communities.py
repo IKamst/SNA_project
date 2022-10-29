599470ded84abb3fff38259311b98c35ed27fe7a
Naomi Broersma <naomibroersma.nb@gmail.com>
2022-10-29 17:13:25 +0200

    Girvan Newman + cliques

--- communities.py
+++ communities.py
@@ -1,88 +1,84 @@
 import copy
+import itertools
+import random
+
 import networkx as nx
+import pandas as pd
 from matplotlib import pyplot as plt
-from networkx import connected_components
-from networkx.algorithms.community import louvain_partitions, modularity
+from networkx import find_cliques
+from networkx.algorithms.community import modularity, girvan_newman
+import seaborn as sns
+
+def make_final_plot(graph, communities):
+    color_map = []
+    get_colours = lambda n: ["#%06x" % random.randint(0, 0xFFFFFF) for _ in range(n)]
+    colours = get_colours(len(communities))
+    for node in graph.nodes():
+        for i in range(len(communities)):
+            if node in communities[i]:
+                color_map.append(colours[i])
+    plt.figure()
+    nx.draw(graph, node_color=color_map, with_labels=True)
+    plt.show()
+    return
 
 
-# TODO calculate betweenness score if we do it ourselves.
-def calculate_betweenness(output):
-    return output
-
-
-# # TODO Calculate the modularity Q.
-# # TODO Repeat from step 2 if: if Q > 0.3 –max Modularity is the best partitioning into clusters
-# def determine_if_best_clusters_own(original_graph, graph):
-#     m = original_graph.number_of_edges()
-#     print(m)
-#     sum_A_min_p = 0
-#     for node_i in graph.nodes():
-#         for node_j in graph.nodes():
-#             A_ij = 0
-#             k_i = original_graph.degree(node_i)
-#             k_j = original_graph.degree(node_j)
-#             p_ij = k_i * k_j / (2 * m)
-#             if graph.has_edge(node_i, node_j):
-#                 A_ij = 1
-#             sum_A_min_p = sum_A_min_p + A_ij - p_ij
-#     modularity = sum_A_min_p / (2 * m)
-#     print("Modularity own: " + str(modularity))
-#     return False
-
-
-def determine_if_best_clusters(original_graph, updated_graph):
-    connected = connected_components(updated_graph)
-    max_modularity = modularity(original_graph, connected)
-    print("Modularity:" + str(max_modularity))
-    return False, max_modularity
-
-
-def girvan_newman_step(graph):
-    betweenness_dict = nx.edge_betweenness_centrality(graph)
-    # TODO calculate betweenness score ourselves?
-    # output = 1
-    # # Loop over all edges.
-    # for edge in graph.edges():
-    #     print(edge)
-    #     # For each edge calculate the betweenness score
-    #     betweenness = calculate_betweenness(output)
-    #     betweenness_dict[edge] = betweenness
-    #     output = output + 1
-    # Sort the dictionary to get the edge with the highest betweenness score.
-    # TODO remove random best if multiple best.
-    sorted_scores = sorted(betweenness_dict.items(), key=lambda x: x[1], reverse=True)
-    print(sorted_scores)
-    best_edge = sorted_scores[0][0]
-    # Remove the edge with the highest betweenness score
-    graph.remove_edge(best_edge[0], best_edge[1])
-    print(graph)
-    return graph
-
-
-def girvan_newman(graph):
+def girvan_newman_all(graph):
     temp_graph = copy.deepcopy(graph)
     best = copy.deepcopy(graph)
     best_clusters = False
     max_mod = 0
-    while temp_graph.number_of_edges() > 0 and not best_clusters:
-        temp_graph = girvan_newman_step(temp_graph)
-        if temp_graph.number_of_edges() > 0:
-            best_clusters, mod = determine_if_best_clusters(graph, temp_graph)
-            if mod > max_mod:
-                max_mod = mod
-                best = copy.deepcopy(temp_graph)
-    print("max: " + str(max_mod))
-    nx.draw_networkx(best)
+    gn_model = girvan_newman(temp_graph)
+    # TODO determine how many runs
+    limited_runs = itertools.takewhile(lambda c: len(c) <= graph.number_of_nodes(), gn_model)
+    for communities in limited_runs:
+        print(tuple(sorted(c) for c in communities))
+        mod = modularity(graph, communities)
+        print("Modularity:" + str(mod))
+        if mod > max_mod:
+            max_mod = mod
+            best = copy.deepcopy(communities)
+    return best, max_mod
+
+
+def cliques(graph):
+    # TODO ideas on how to plot this?
+    max_cliques = list(find_cliques(graph))
+    print("Maximal cliques: " + str(max_cliques))
+    nodes = []
+    cnt_cliques = []
+    for node in graph.nodes():
+        cnt = 0
+        for clique in max_cliques:
+            if node in clique:
+                cnt = cnt + 1
+        cnt_cliques.append(cnt)
+        nodes.append(str(nodes))
+    df = pd.DataFrame({"Node": nodes,
+                       "Number of cliques": cnt_cliques})
+    df.sort_values("Number of cliques")
+    plt.figure()
+    sns.barplot(x='Node', y="Number of cliques", data=df)
     plt.show()
     return
 
 
 def main():
-    graph = nx.karate_club_graph()
+    graph = nx.random_k_out_graph(20, 3, 0.5)
     nx.draw_networkx(graph)
-    plt.savefig("graph.png")
     plt.show()
-    girvan_newman(graph)
+    # Create an undirected version of this graph.
+    # reciprocal: bool (optional) (if True only keep edges that appear in both directions).
+    undirected_graph = graph.to_undirected()
+    nx.draw_networkx(undirected_graph)
+    plt.show()
+    # Find the maximal cliques
+    cliques(undirected_graph)
+    # Find the best partitioning using Girvan Newman with modularity
+    best, max_mod = girvan_newman_all(undirected_graph)
+    print("Maximum modularity: " + str(max_mod))
+    print("For best communities partitioning: " + str(best))
+    make_final_plot(undirected_graph, best)
 
 
 if __name__ == "__main__":
