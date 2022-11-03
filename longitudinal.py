7617f2c225a95f8898a090d886f263edb992f419
BVJongsma <b.v.jongsma@student.rug.nl>
2022-11-03 15:48:36 +0100

    started create_timeline() It can make a list of times now from source-tweets. still to do: - order this list - put ID's in - put reaction-tweets as well.

--- longitudinal.py
+++ longitudinal.py
@@ -1,14 +1,9 @@
 from json import load, dump
-from os import getcwd
-
-
-def longitudinal_analysis(graph):
-    test()
-    return
-
+import os
+import datetime
 
 def test():
-    wd = getcwd()
+    wd = os.getcwd()
     file_path = wd + '/germanwings-crash-all-rnr-threads/non-rumours/580319983676313601/reactions/580325737619685377.json'
     f = open(file_path)
     data = load(f)
@@ -16,3 +11,53 @@ def test():
     dump(data, out_file, indent="")
     print(data["created_at"])
     return
+
+
+#input: fp = filepath of the .json file
+#output: <class 'datetime.datetime'>
+# reference: https: // www.geeksforgeeks.org / python - time - strptime - function /  #:~:text=The%20strptime()%20function%20in,according%20to%20the%20provided%20directives.
+def scrap_time_from_file(fp):
+    f = open(fp)
+    data = load(f)
+    datetime_obj = datetime.datetime.strptime(
+        data["created_at"], '%a %b %d %H:%M:%S %z %Y')
+    return datetime_obj
+
+#https://stackoverflow.com/questions/3121979/how-to-sort-a-list-tuple-of-lists-tuples-by-the-element-at-a-given-index
+def sort_timeline(times):
+    return times
+
+
+def create_timeline():
+    wd = os.getcwd()
+    path = wd + '/germanwings-crash-all-rnr-threads' + '/non-rumours'
+    times = []
+    #'/germanwings-crash-all-rnr-threads/non-rumours/580319983676313601/reactions/580325737619685377.json')
+    for directory_name in os.listdir(path): #directory_name = 580319983676313601
+        #temporary: TODO remove this if-statement
+        if directory_name == '580319983676313601':
+            direc_path = path + '/'+ directory_name
+            if os.path.isdir(direc_path):
+                # Loop over the files in that directory.
+                for file in os.listdir(direc_path + '/source-tweets'):
+                    file_path = os.path.join(direc_path + '/source-tweets', file)
+                    if file == (directory_name + '.json'): #if source
+                        times.append(scrap_time_from_file(file_path))
+            for file in os.listdir(direc_path + '/reactions'):
+                    pass
+                    # file_path = os.path.join(direc_path, file)
+                    # if os.path.isfile(file_path):
+                    #     if file == directory_name:
+                    #         dt = scrap_time_from_file(file_path)
+            pass
+    print(times)
+    times = sort_timeline(times)
+    dt = scrap_time_from_file(wd + '/germanwings-crash-all-rnr-threads/non-rumours/580319983676313601/reactions/580325737619685377.json')
+    return times
+
+
+def longitudinal_analysis(graph):
+    test()
+    create_timeline()
+    return
+
