c7013dc7e20d4a7e71bfd8c505f78bf33a96256f
BVJongsma <b.v.jongsma@student.rug.nl>
2022-11-03 17:17:32 +0100

    ordered list and put ID'S in

--- longitudinal.py
+++ longitudinal.py
@@ -2,60 +2,46 @@ from json import load, dump
 import os
 import datetime
 
-def test():
-    wd = os.getcwd()
-    file_path = wd + '/germanwings-crash-all-rnr-threads/non-rumours/580319983676313601/reactions/580325737619685377.json'
-    f = open(file_path)
-    data = load(f)
-    out_file = open(wd + "/testing_dictionary" + ".json", "w")
-    dump(data, out_file, indent="")
-    print(data["created_at"])
-    return
-
-
-#input: fp = filepath of the .json file
-#output: <class 'datetime.datetime'>
+EGO = '580341214098137089'
+# input: fp = filepath of the .json file
+# output: <class 'datetime.datetime'>
 # reference: https: // www.geeksforgeeks.org / python - time - strptime - function /  #:~:text=The%20strptime()%20function%20in,according%20to%20the%20provided%20directives.
 def scrap_time_from_file(fp):
     f = open(fp)
     data = load(f)
     datetime_obj = datetime.datetime.strptime(
         data["created_at"], '%a %b %d %H:%M:%S %z %Y')
-    return datetime_obj
-
-#https://stackoverflow.com/questions/3121979/how-to-sort-a-list-tuple-of-lists-tuples-by-the-element-at-a-given-index
-def sort_timeline(times):
-    return times
+    return datetime_obj, data["id"], 0
 
 
 def create_timeline():
     wd = os.getcwd()
-    path = wd + '/germanwings-crash-all-rnr-threads' + '/non-rumours'
+    base = wd + '/germanwings-crash-all-rnr-threads'
     times = []
-    #'/germanwings-crash-all-rnr-threads/non-rumours/580319983676313601/reactions/580325737619685377.json')
-    for directory_name in os.listdir(path): #directory_name = 580319983676313601
-        #temporary: TODO remove this if-statement
-        if directory_name == '580319983676313601':
-            direc_path = path + '/'+ directory_name
-            if os.path.isdir(direc_path):
-                # Loop over the files in that directory.
-                for file in os.listdir(direc_path + '/source-tweets'):
-                    file_path = os.path.join(direc_path + '/source-tweets', file)
-                    if file == (directory_name + '.json'): #take only the source tweet file
-                        # TODO: make this a list of tuples, including ID
-                        times.append(scrap_time_from_file(file_path))
-                for file in os.listdir(direc_path + '/reactions') :
-                    file_path = os.path.join(direc_path + '/reactions', file)
-                    if '_' not in file:  # only take tweets
-                        times.append(scrap_time_from_file(file_path))
-    print(times)
-    times = sort_timeline(times)
-    dt = scrap_time_from_file(wd + '/germanwings-crash-all-rnr-threads/non-rumours/580319983676313601/reactions/580325737619685377.json')
+    for rumourfolder in os.listdir(base):  # non-rumour and rumour
+        path = base + '/' + rumourfolder
+        if os.path.isdir(path):
+            for directory_name in os.listdir(path):
+                if directory_name == EGO:# directory_name = 580319983676313601
+                    direc_path = path + '/' + directory_name
+                    if os.path.isdir(direc_path):
+                        # Loop over the files in that directory.
+                        for file in os.listdir(direc_path + '/source-tweets'):
+                            file_path = os.path.join(direc_path + '/source-tweets', file)
+                            if file == (directory_name + '.json'):  # take only the source tweet file
+                                times.append(scrap_time_from_file(file_path))
+                        if os.path.exists(direc_path + '/reactions'):
+                            for file in os.listdir(direc_path + '/reactions'):
+                                file_path = os.path.join(direc_path + '/reactions', file)
+                                if '_' not in file:  # only take tweets
+                                    times.append(scrap_time_from_file(file_path))
+    # sort it by time
+    times.sort(key=lambda tup: tup[0])
+
     return times
 
 
-def longitudinal_analysis(graph):
-    test()
-    create_timeline()
+def longitudinal_analysis():
+    timeline = create_timeline()
+    print(len(timeline))
     return
-
--- main.py
+++ main.py
@@ -59,16 +59,13 @@ def main():
         print("The structure files have been created. Please set OPEN_STRUCTURE_FILES to True.")
     else:
         print(g)
-        nx.draw_networkx(g, node_size=20, with_labels=False)
-        plt.title("Directed version of the network")
-        plt.show()
-        calculate_metrics(g)
-        calculate_hits(g)
-        community_analysis(g)
-        # calculate_metrics(g)
+        # nx.draw_networkx(g, node_size=20, with_labels=False)
+        # plt.title("Directed version of the network")
+        # plt.show()
+        #calculate_metrics(g)
         # calculate_hits(g)
         # community_analysis(g)
-        longitudinal_analysis(g)
+        longitudinal_analysis()
 
 
 if __name__ == "__main__":
