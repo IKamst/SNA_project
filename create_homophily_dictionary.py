dcee3173262efe4ed1a5769c4403539f6f62ae23
Naomi Broersma <naomibroersma.nb@gmail.com>
2022-11-04 22:44:35 +0100

    Done commenting create_homophily_dictionary.py and communities.py

--- create_homophily_dictionary.py
+++ create_homophily_dictionary.py
@@ -1,14 +1,12 @@
 import json
 import os
-import re
-
-import networkx as nx
 
 
 # Reads the data from the structure.json file.
 def read_data_homophily(rumourboolpath, id_to_verified_dic):
     wd = os.getcwd()
     path = wd + '/germanwings-crash-all-rnr-threads/' + rumourboolpath
+    # Loop over the directories of each tweet.
     for directory_name in os.listdir(path):
         direc_path = os.path.join(path, directory_name)
         if os.path.isdir(direc_path):
@@ -17,12 +15,16 @@ def read_data_homophily(rumourboolpath, id_to_verified_dic):
     return id_to_verified_dic
 
 
+# Create a dictionary of user id with whether they are verified or not.
 def determine_verified_dic(directory, tweet_to_id_dic):
+    # Loop over reactions and source-tweets directories.
     for directory_name in os.listdir(directory):
         direc_path = os.path.join(directory, directory_name)
         if os.path.isdir(direc_path):
+            # Loop over the tweets.
             for file in os.listdir(direc_path):
                 if '_' not in file:
+                    # Open the file and load the data.
                     file_path = os.path.join(direc_path, file)
                     f = open(file_path)
                     # Load the data as a dictionary.
@@ -30,10 +32,12 @@ def determine_verified_dic(directory, tweet_to_id_dic):
                     user = data["user"]
                     id_person = user["id"]
                     verified = user["verified"]
+                    # Save whether the account is verified to the id.
                     tweet_to_id_dic[id_person] = verified
     return tweet_to_id_dic
 
 
+# Small function to try out the code.
 if __name__ == "__main__":
     id_to_verified_dic = {}
     id_to_verified_dic = read_data_homophily('non-rumours', id_to_verified_dic)
